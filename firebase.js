72d39c9808890eb92d7516ee4d9a0dc4151b258c
Szymon Nowak <sszymon.nnowak@gmail.com>
2026-02-16 20:33:40 +0100

    Added Firebase to env

--- firebase.js
+++ firebase.js
@@ -5,13 +5,22 @@ import { getFirestore } from 'firebase/firestore';
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID
+} from '@env';
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCytrw08ivmpNDrfnw6xvaRkmyUgECqAIk",
-  authDomain: "ideabrew.firebaseapp.com",
-  projectId: "ideabrew",
-  storageBucket: "ideabrew.firebasestorage.app",
-  messagingSenderId: "489706869257",
-  appId: "1:489706869257:web:8120553328e400e0185ed8"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
